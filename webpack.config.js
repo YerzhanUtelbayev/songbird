2459d97a48f3646922d19cc6a9cfa1a98cc4a6b9
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-08-03 19:05:22 +0600

    config: add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -120,5 +120,12 @@ module.exports = function (_env, argv) {
       },
       runtimeChunk: 'single',
     },
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      overlay: true,
+      port: 8080,
+    },
   };
 };
