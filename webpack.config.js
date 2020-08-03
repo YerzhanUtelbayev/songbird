de9122055a23b133182a9a5545b5eeef0f951bc8
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-08-03 17:49:41 +0600

    config: configure plain styles loading

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = function (_env, argv) {
+  const isProduction = argv.mode === 'production';
+  const isDevelopment = !isProduction;
+
+  return {
+    devTool: isDevelopment && 'cheap-module-source-map',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: './src/index.jsx',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/js/[name].[contenthash:8].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: babel - loader,
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? 'production' : 'development',
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      isProduction && new MiniCssExtractPlugin({
+        filename: 'assets/css/[name].[contenthash:8].css',
+        chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
+      }),
+      new webpack.ProgressPlugin(),
+      new HTMLWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+    ].filter(Boolean),
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
+};
