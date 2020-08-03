a9327a7539e6d0593e53e775cf3e84d4f51fa42e
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-08-03 18:38:26 +0600

    config: add code splitting and minification

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = function (_env, argv) {
   const isProduction = argv.mode === 'production';
@@ -55,6 +57,9 @@ module.exports = function (_env, argv) {
         },
       ],
     },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
     plugins: [
       isProduction &&
         new MiniCssExtractPlugin({
@@ -66,9 +71,54 @@ module.exports = function (_env, argv) {
         template: path.resolve(__dirname, 'public/index.html'),
         inject: true,
       }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          isProduction ? 'production' : 'development'
+        ),
+      }),
     ].filter(Boolean),
-    resolve: {
-      extensions: ['.js', '.jsx'],
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              comparisons: false,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              comments: false,
+              ascii_only: true,
+            },
+            warnings: false,
+          },
+        }),
+        new OptimizeCssAssetsPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `${cacheGroupKey}.${packageName.replace('@', '')}`;
+            },
+            common: {
+              minChunks: 2,
+              priority: -10,
+            },
+          },
+        },
+      },
+      runtimeChunk: 'single',
     },
   };
 };
